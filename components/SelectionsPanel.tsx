39ca978f4efafad0da72bc3be0db1960693215d6
Wolfazzo <ronnydaca@gmail.com>
2025-11-09 20:52:37 +0100

    Updates

--- components/SelectionsPanel.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This component is currently unused and has been disabled to prevent potential build issues.
-// Its dependencies were causing crashes when types were missing.
-const SelectionsPanel: React.FC = () => {
-    return null;
-};
-
-export default SelectionsPanel;
